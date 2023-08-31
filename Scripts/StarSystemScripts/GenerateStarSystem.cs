d1f0c195e9b4a67b22782bfd2eaed4d4bdef91a5
Lawfty Goals <mymsnadressis@hotmail.com>
2023-09-01 00:05:20 +0200

    start starsystem work

--- Scripts/StarSystemScripts/GenerateStarSystem.cs
+++ Scripts/StarSystemScripts/GenerateStarSystem.cs
@@ -3,5 +3,12 @@ using System;
 
 public partial class GenerateStarSystem : Node
 {
+    private int sectorSize = 1000;
+    private int numberOfSectors;
+
+    private Godot.Vector2 centerPoint = new Vector2(0, 0);
+
+    private void currentSector() { }
+
     public override void _Ready() { }
 }
